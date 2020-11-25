f3625b67846a5a5fe45888157cdce467629147ed
Rekfuby <trollebane@gmail.com>
2020-11-25 23:25:54 +0900

    added basic ui

---
+++ Project F/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    
+	public void LoadLevel(string levelName) 
+	{
+		SceneManager.LoadSceneAsync(levelName);
+	}
+	
+	public void ExitGame() 
+	{
+		Application.Quit();
+	}
+	
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
