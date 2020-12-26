04762133f2fb38aae0f700877c1560d4623547b7
Rekfuby <trollebane@gmail.com>
2020-12-26 11:14:24 +0900

    added 2nd level manager

--- Project F/Assets/Scripts/BlastDoor.cs
+++ Project F/Assets/Scripts/BlastDoor.cs
@@ -7,6 +7,7 @@ public class BlastDoor : MonoBehaviour
 	bool opened = false;
 	Vector2 playerOffPos;
 	BoxCollider2D collider2d;
+	public bool locked = true;
 	
 	private GameObject playerObj = null;
 	public Animator animator;
@@ -25,20 +26,28 @@ public class BlastDoor : MonoBehaviour
 		playerOffPos = (Vector2)playerObj.transform.position;
 		if (Input.GetButtonDown("Use") && playerCloseToObject())
 		{
-			if (!opened)
+			if (!locked)
 			{
-				animator.SetTrigger("Open");
-				opened = true;
-				animator.SetBool("isOpened", true);
-				collider2d.enabled = false;
+					if (!opened)
+				{
+					animator.SetTrigger("Open");
+					opened = true;
+					animator.SetBool("isOpened", true);
+					collider2d.enabled = false;
+				}
+				else
+				{
+					animator.SetTrigger("Close");
+					opened = false;
+					animator.SetBool("isOpened", false);
+					collider2d.enabled = true;
+				}
 			}
 			else
 			{
-				animator.SetTrigger("Close");
-				opened = false;
-				animator.SetBool("isOpened", false);
-				collider2d.enabled = true;
+				// locked door audio here
 			}
+			
 		}
     }
 	
--- Project F/Assets/Scripts/LabsManager.cs
+++ Project F/Assets/Scripts/LabsManager.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class LabsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+	private int doorNum = 12;
+	private GameObject[] bds = new GameObject[12];
+	
+	
     void Start()
     {
-        
+        for(int i = 0; i < doorNum; i++)
+		{
+			bds[i] = GameObject.Find(string.Format("BlastDoor{0}", i + 1));
+			Debug.Log(bds[i]);
+		}
+		bds[0].GetComponent<BlastDoor>().locked = false;
+		bds[1].GetComponent<BlastDoor>().locked = false;
+		bds[6].GetComponent<BlastDoor>().locked = false;
+		bds[8].GetComponent<BlastDoor>().locked = false;
+		bds[9].GetComponent<BlastDoor>().locked = false;
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
